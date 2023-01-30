f7999b8b1919849b37cd21b87dfcd55474bd1ae9
PxperMoon <89617671+PxperMoon@users.noreply.github.com>
2023-01-29 21:41:34 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -3,10 +3,18 @@ function preload() {
 }
 
 function setup() {
-canvas = createCanvas(300, 300);
-canvas.center();
-canvas.display();
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
 }
+
+function modelLoaded() {
+    console.log("PoseNet is Initialized.");
+} 
 
 function draw {
 
@@ -14,4 +22,17 @@ function draw {
 
 function take_snapshot() {
 
+}
+
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+
+        Nosex = results[0].pose.nose.x;
+        Nosey = results[0].pose.nose.y;
+
+        console.log("nose x = " + Nosex);
+        console.log("nose y = " + Nosey);
+    }
 }
