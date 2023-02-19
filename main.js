1a7279adb2a2b01448d709e985665178abace7e5
PxperMoon <89617671+PxperMoon@users.noreply.github.com>
2023-02-19 14:13:56 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-function preload() {
+NoseX = 0;
+NoseY = 0;
 
+function preload() {
+    lipstick = loadImage('glossy-lipss-removebg-preview.png');
 }
 
 function setup() {
     canvas = createCanvas(300, 300);
     canvas.center();
+    background("white");
     video = createCapture(VIDEO);
     video.size(300, 300);
     video.hide();
@@ -16,8 +20,10 @@ function modelLoaded() {
     console.log("PoseNet is Initialized.");
 } 
 
-function draw {
+function draw() {
+    image(video, 0, 0, 300, 300);
 
+    image(lipstick, NoseX-31, NoseY, 70, 70);
 }
 
 function take_snapshot() {
@@ -29,10 +35,10 @@ function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
 
-        Nosex = results[0].pose.nose.x;
-        Nosey = results[0].pose.nose.y;
+        NoseX = results[0].pose.nose.x;
+        NoseY = results[0].pose.nose.y;
 
-        console.log("nose x = " + Nosex);
-        console.log("nose y = " + Nosey);
+        console.log("nose x = " + NoseX);
+        console.log("nose y = " + NoseY);
     }
 }
